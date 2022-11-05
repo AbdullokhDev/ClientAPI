5b0c1d89424420a9cb8a2a3cbdd462c5ea9a521b
Abdullokh <abdulloh.watch@internet.ru>
2022-11-05 16:33:30 +0500

    init

---
+++ Migrations/20221030074519_firstmigration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Client.Migrations
+{
+    public partial class firstmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    firstName = table.Column<string>(type: "text", nullable: false),
+                    lastName = table.Column<string>(type: "text", nullable: false),
+                    phoneNumber = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    address = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Logging.AddSimpleConsole();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Connect to PostgreSQL Database
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ClientDb>(options =>
+    options.UseNpgsql(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Create new client
+app.MapPost("/clients/", async(Klient n, ClientDb db)=> {
+    db.Clients.Add(n);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/clients/{n.id}", n);
+});
+
+// Get list of clients
+app.MapGet("/clients", async (ClientDb db) => await db.Clients.ToListAsync());
+
+// Get single client by id
+app.MapGet("/clients/{id:int}", async(int id, ClientDb db)=> 
+{
+    return await db.Clients.FindAsync(id)
+            is Klient n
+                ? Results.Ok(n)
+                : Results.NotFound();
+});
+
+// Update client
+app.MapPut("/clients/{id:int}", async(int id, Klient n, ClientDb db)=>
+{
+    if (n.id != id)
+    {
+        return Results.BadRequest();
+    }
+
+    var client = await db.Clients.FindAsync(id);
+    
+    if (client is null) return Results.NotFound();
+
+    //found, so update with incoming note n.
+    client.firstName = n.firstName;
+    client.lastName = n.lastName;
+    client.email = n.email;
+    client.phoneNumber = n.phoneNumber;
+    client.address = n.address;
+    await db.SaveChangesAsync();
+    return Results.Ok(client);
+});
+
+// Delete client
+app.MapDelete("/clients/{id:int}", async(int id, ClientDb db)=>{
+
+    var note = await db.Clients.FindAsync(id);
+    if (note is not null){
+        db.Clients.Remove(note);
+        await db.SaveChangesAsync();
+    }
+    return Results.NoContent();
+});
+
+
+app.Run();
+
+// Creating Database tables with Entity Framework (ORM)
+record Klient(int id){
+    public string firstName { get; set; } = default!;
+    public string lastName { get; set; } = default!;
+    public string phoneNumber { get; set; } = default!;
+    public string email { get; set; } = default!;
+    public string address { get; set; } = default!;
+}
+
+class ClientDb: DbContext {
+    public ClientDb(DbContextOptions<ClientDb> options): base(options) {
+
+    }
+    public DbSet<Klient> Clients => Set<Klient>();
+}
